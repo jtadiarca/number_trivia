018ac9b6e33098d36af4a6c531db0793b98486ce
Joseph Tadiarca <avegailtadiarca@gmail.com>
2020-09-27 21:08:07 -0500

    Added dependency injection

---
+++ lib/injection_container.dart
@@ -0,0 +1,57 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'core/network/network_info.dart';
+import 'features/number_trivia/data/datasources/number_trivia_local_datasource.dart';
+import 'features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
+import 'features/number_trivia/data/repositories/number_trivia_respository_impl.dart';
+import 'features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'features/number_trivia/domain/usecases/concrete_number_trivia.dart';
+import 'features/number_trivia/domain/usecases/random_number_trivia.dart';
+import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+
+final serviceLocator = GetIt.instance;
+
+void init() async {
+  //! Features - Number Trivia
+  // Bloc
+  serviceLocator.registerFactory(
+    () => NumberTriviaBloc(
+      concreteNumberTrivia: serviceLocator(),
+      randomNumberTrivia: serviceLocator(),
+    ),
+  );
+
+  // User cases
+  serviceLocator
+      .registerLazySingleton(() => ConcreteNumberTrivia(serviceLocator()));
+  serviceLocator
+      .registerLazySingleton(() => RandomNumberTrivia(serviceLocator()));
+
+  // Repository
+  serviceLocator.registerLazySingleton<NumberTriviaRepository>(
+    () => NumberTriviaRepositoryImpl(
+      localDataSource: serviceLocator(),
+      networkInfo: serviceLocator(),
+      remoteDataSource: serviceLocator(),
+    ),
+  );
+
+  // Data sources
+  serviceLocator.registerLazySingleton<NumberTriviaRemoteDataSource>(
+      () => NumberTriviaRemoteDataSourceImpl(client: serviceLocator()));
+  serviceLocator.registerLazySingleton<NumberTriviaLocalDataSource>(() =>
+      NumberTriviaLocalDataSourceImpl(sharedPreferences: serviceLocator()));
+
+  //! Core
+  serviceLocator.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(serviceLocator()));
+
+  //! External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  serviceLocator.registerLazySingleton(() => sharedPreferences);
+  serviceLocator.registerLazySingleton(() => http.Client());
+  serviceLocator.registerLazySingleton(() => DataConnectionChecker());
+}
